e8b773c7eddf0c949660d6e7b91a7fd57db60c31
Unknown <superregmail@gmx.de>
2017-12-17 02:09:43 +0100

    Major change in usage and basic error catching

--- src/live_conkydb.py
+++ src/live_conkydb.py
@@ -6,7 +6,7 @@ from conkyutil.writer import ConkyWriter
 from os.path import expanduser
 import urllib2
 import netifaces
-import re
+import re, os
 
 
 def departure_monitor(station):
@@ -17,7 +17,7 @@ def departure_monitor(station):
         tree = ET.fromstring(xml)
         sid = tree.get('sessionID') #sessionID to get departure Data
         #Secound Request
-        response2 = urllib2.urlopen('http://www.efa-bw.de/nvbw/XML_DM_REQUEST?sessionID='+sid+'&requestID=1&dmLineSelection=4:x')
+        response2 = urllib2.urlopen('http://www.efa-bw.de/nvbw/XML_DM_REQUEST?sessionID='+sid+'&requestID=1&dmLineSelectionAll=1')
         xml2 = response2.read()
         departures_tree = ET.fromstring(xml2)
         hours = departures_tree.find('itdDepartureMonitorRequest').find('itdDateTime').find('itdTime').get('hour')
@@ -28,11 +28,15 @@ def departure_monitor(station):
             minutes = '0'+minutes
         time = hours+':'+minutes
         station_name = departures_tree.find('itdDepartureMonitorRequest').find('itdOdv').find('itdOdvName').find('odvNameElem').text
-
+        #print station_name
+        if station_name is None:
+            station_name = departures_tree.find('itdDepartureMonitorRequest').find('itdOdv').find('itdOdvName').find('odvNameInput').text
         output.append("${font}${font Poiret One:size=15}"+station_name.encode("UTF8")+"${font}${font DejaVu Sans Mono:size=10}")
         #writer.voffset(50).offset(12).color('white').write().newline()
 
         count = 1
+        #print departures_tree.find('itdDepartureMonitorRequest').find('itdDepartureList')
+
         for itdDeparture in departures_tree.find('itdDepartureMonitorRequest').find('itdDepartureList'):
             abfahrt_class = ""
             abfahrt = itdDeparture.get('countdown')
@@ -42,7 +46,11 @@ def departure_monitor(station):
             if count > 6:
                 break
             #writer.voffset(12).offset(12).color('white').write(line + " " + destination.encode("UTF8") + " " + abfahrt).newline()
-            output.append("\\" + '{:>2}'.format(line)  +" "+ '{:<30}'.format(destination.encode("UTF8")) +" "+'{:>3}'.format(abfahrt))
+            #Print Line just letters if string is longer than 3 symbols
+            #count special characters in destination an add to printlengh
+            output.append("\\- " + '{:<4}'.format(line[:4])  +" "+ '{:<30}'.format(destination.encode("UTF8")) +" "+'{:>3}'.format(abfahrt))
+        if count < 5:
+            output.append("\Does not support standard EFA!")
 
     except Exception, exc:
         print "Something went wrong!"
@@ -54,14 +62,16 @@ def get_station_db():
     router_mac = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", Popen(["arp", "-a"], stdout=PIPE).communicate()[0]).groups()[0]
     wireless = Wireless()
     ss_id = wireless.current()
-    db = TinyDB('../db/db.json')
+    if os.path.dirname(__file__) is not '':
+        db = TinyDB(os.path.dirname(__file__)+'/../db/db.json')
+    else:
+        db = TinyDB('../db/db.json')
     station_query = Query()
-    #print db.search((station_query.SSID == ss_id)&(station_query.MAC == router_mac))
+
     try:
         result = db.search((station_query.SSID == ss_id)&(station_query.MAC == router_mac))
         stationlist = []
         for item in result:
-            #print  item.encode('UTF8')
             stationlist.append(item['Station'])
         return stationlist
     except:
@@ -72,13 +82,15 @@ printout = []
 for item in station_ids:
     printout += departure_monitor(item)
 #print printout
-with open(expanduser("~")+"/.conky_departure_monitor","w+") as conkyrc:
-    conkyrc.write("conky.config = {background=true,double_buffer=true,no_buffers=true,imlib_cache_size=10,draw_shades=false,draw_outline=false,use_xft=true,xftalpha=1,font='Droid Sans:size=10',text_buffer_size=300,override_utf8_locale=true,gap_x=0,gap_y=0,alignment='middle_right',own_window=true,own_window_type='desktop',own_window_transparent=true,own_window_hints='undecorated,below,sticky,skip_taskbar,skip_pager',own_window_argb_visual=true,own_window_argb_value=0,}conky.text = [[ ${font Droid Sans:size=14}           << DEPARTURES >>${font}${font Poiret One:size=15}\n------------------------------------------")
+writer = ConkyWriter()
+#
+# with open(expanduser("~")+"/.conky_departure_monitor","w+") as conkyrc:
+#     conkyrc.write("conky.config = {background=true,double_buffer=true,no_buffers=true,imlib_cache_size=10,draw_shades=false,draw_outline=false,use_xft=true,xftalpha=1,font='Droid Sans:size=10',text_buffer_size=300,override_utf8_locale=true,gap_x=0,gap_y=0,alignment='middle_right',own_window=true,own_window_type='desktop',own_window_transparent=true,own_window_hints='undecorated,below,sticky,skip_taskbar,skip_pager',own_window_argb_visual=true,own_window_argb_value=0,}conky.text = [[ ${font Droid Sans:size=14}           << DEPARTURES >>${font}${font Poiret One:size=15}\n------------------------------------------")
 
-writer = ConkyWriter(open(expanduser("~")+"/.conky_departure_monitor","a"))
+#writer = ConkyWriter(open(expanduser("~")+"/.conky_departure_monitor","a"))
 for item in printout:
-    writer.newline()
     writer.voffset(12).offset(12).color('white').write(item)
+    writer.newline()
 writer.newline()
 writer.newline()
-writer.voffset(12).offset(12).color('white').write("${font}]]")
+#writer.voffset(12).offset(12).color('white').write("${font}]]")
